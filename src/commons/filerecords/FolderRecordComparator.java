24e8393e10d4006ee1c784b89dfa0fa07b0fe95e
Alron <alronjan@yahoo.com>
2014-12-09 18:33:46 +0800

    Implemented FolderRecordComparator (untested). The base classes are there but are never used yet.

--- src/commons/filerecords/FolderRecordComparator.java
+++ src/commons/filerecords/FolderRecordComparator.java
@@ -1,18 +1,15 @@
 package commons.filerecords;
 
 import java.net.Socket;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
+import job.CreateJob;
+import job.DeleteJob;
 import job.Job;
 
 public class FolderRecordComparator {
-	/***
-	 * This object is considered as the latest record, while the otherFolder is
-	 * considered as the older record.
-	 * 
-	 * @param otherFolder
-	 * @return
-	 */
+
 	public ArrayList<Job> compareAndGenerateJobs(FolderRecord oldRecord, FolderRecord newRecord, Socket serverSocket) {
 		ArrayList<Job> jobs = new ArrayList<Job>();
 		jobs.addAll(this.generateCreateJobs(oldRecord, newRecord, serverSocket));
@@ -29,8 +26,9 @@ public class FolderRecordComparator {
 
 		for (FileRecord newFileRecord : newFileRecords) {
 			if (!oldFileRecords.contains(newFileRecord)) {
-
-				// createJobs.add(new Job());
+				// placed dummy because i currently have no access to the
+				// calling app's shared folder
+				createJobs.add(new CreateJob(Paths.get("dummy/" + newFileRecord.getFileName()), serverSocket));
 			}
 		}
 
@@ -40,11 +38,42 @@ public class FolderRecordComparator {
 	private ArrayList<Job> generateModifyJobs(FolderRecord oldRecord, FolderRecord newRecord, Socket serverSocket) {
 		ArrayList<Job> modifyJobs = new ArrayList<Job>();
 
+		ArrayList<FileRecord> newFileRecords = newRecord.getList();
+		ArrayList<FileRecord> oldFileRecords = oldRecord.getList();
+
+		for (FileRecord newFileRecord : newFileRecords) {
+			int matchIndex = oldFileRecords.indexOf(newFileRecord);
+
+			if (matchIndex >= 0) {
+				FileRecord matchInOldRecords = oldFileRecords.get(matchIndex);
+				if (newFileRecord.getDateTimeModified() > matchInOldRecords.getDateTimeModified()) {
+					// placed dummy because i currently have no access to the
+					// calling app's shared folder
+					modifyJobs.add(new CreateJob(Paths.get("dummy/" + newFileRecord.getFileName()), serverSocket));
+				}
+
+			}
+		}
+
 		return modifyJobs;
 	}
 
 	private ArrayList<Job> generateDeleteJobs(FolderRecord oldRecord, FolderRecord newRecord, Socket serverSocket) {
 		ArrayList<Job> deleteJobs = new ArrayList<Job>();
+
+		ArrayList<FileRecord> newFileRecords = newRecord.getList();
+		ArrayList<FileRecord> oldFileRecords = oldRecord.getList();
+
+		for (FileRecord oldFileRecord : oldFileRecords) {
+			int matchIndex = newFileRecords.indexOf(oldFileRecord);
+
+			if (matchIndex < 0) {
+				// placed dummy because i currently have no access to the
+				// calling app's shared folder
+				deleteJobs.add(new DeleteJob(Paths.get("dummy/" + oldFileRecord.getFileName()), oldRecord
+						.getTimeLastModified(), serverSocket));
+			}
+		}
 
 		return deleteJobs;
 	}
