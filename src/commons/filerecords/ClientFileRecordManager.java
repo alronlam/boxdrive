8dada732df2fdc1bcf0bf28cf83e6cab17ec8756
Alron <alronjan@yahoo.com>
2014-12-10 00:47:44 +0800

    Added methods for initializing ClientFileRecordManager. This still has no access to the client's directory (it's hardcoded for now). Also, serialized records will also just be placed in the root folder. But this will cause conflicts if we test multiple client/server instances within the same root folder. Lastly, the method serializeList() which saves the record is never called as of this version.

--- src/commons/filerecords/ClientFileRecordManager.java
+++ src/commons/filerecords/ClientFileRecordManager.java
@@ -1,5 +1,20 @@
 package commons.filerecords;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -15,6 +30,63 @@ public class ClientFileRecordManager {
 
 	private ClientFileRecordManager() {
 
+		String directory = "client1"; // temporary. will need to access
+										// the client's directory
+		String recordFileName = "file_records.ser";
+
+		// try to read from serialized list
+		// if not successful, then init the records based on directory
+		ArrayList<FileRecord> records = this.readFromSerializedList(recordFileName);
+		if (records == null)
+			records = this.initRecordsBasedOnDirectory(directory);
+
+	}
+
+	private ArrayList<FileRecord> readFromSerializedList(String filePath) {
+		// deserialize the quarks.ser file
+		try (InputStream file = new FileInputStream(filePath);
+				InputStream buffer = new BufferedInputStream(file);
+				ObjectInput input = new ObjectInputStream(buffer);) {
+			// deserialize the List
+			ArrayList<FileRecord> records = (ArrayList<FileRecord>) input.readObject();
+
+			return records;
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		return null;
+	}
+
+	private void serializeList(String filePath) {
+		// serialize the List
+		try (OutputStream file = new FileOutputStream(filePath);
+				OutputStream buffer = new BufferedOutputStream(file);
+				ObjectOutput output = new ObjectOutputStream(buffer);) {
+			output.writeObject(this.list);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private ArrayList<FileRecord> initRecordsBasedOnDirectory(String directory) {
+
+		ArrayList<FileRecord> records = new ArrayList<FileRecord>();
+
+		try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(directory))) {
+			for (Path path : directoryStream) {
+				FileRecord fileRecord = new FileRecord(path.getFileName().toString(), Files.getLastModifiedTime(path)
+						.toMillis());
+				records.add(fileRecord);
+			}
+			Collections.sort(records);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		return records;
 	}
 
 	// The rest of the class
