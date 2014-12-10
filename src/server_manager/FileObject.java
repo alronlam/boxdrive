127b6c6b85033c491407da8d745ff87e0e7c8cc2
chessmotif <chessmotif@gmail.com>
2014-12-10 17:42:37 +0800

    Added skeleton of the multi-server thing.
    Some methods have been added, but this isn't complete yet :\

---
+++ src/server_manager/FileObject.java
@@ -0,0 +1,15 @@
+package server_manager;
+
+import java.nio.file.Path;
+
+public class FileObject {
+	public Path path;
+	public int config;
+	
+	public FileObject(Path p, int c) {
+		path = p;
+		config = c;
+	}
+	
+	// lel
+}
