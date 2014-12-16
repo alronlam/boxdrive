06fd1bc15f5bbda6efef0bef7786c5c8df5fdc6c
Alron <alronjan@yahoo.com>
2014-12-17 00:36:56 +0800

    Added code in ClearSerFilesDriver to also delete the contents of all the folders.

--- src/ClearSerFilesDriver.java
+++ src/ClearSerFilesDriver.java
@@ -4,20 +4,38 @@ import java.io.PrintWriter;
 
 import commons.Constants;
 
-
 public class ClearSerFilesDriver {
 	public static void main(String args[]) throws FileNotFoundException {
 		File dir;
-		String folders[] = { "client1","client2" };
+		String clients[] = { "client1", "client2" };
 
-		for (String dirPath : folders) {
-			dir = new File(dirPath+ "_" +  Constants.FOLDER_RECORD_FILENAME);
+		for (String dirPath : clients) {
+			dir = new File(dirPath + "_" + Constants.FOLDER_RECORD_FILENAME);
 			PrintWriter writer = new PrintWriter(dir);
 			writer.close();
-			
-			System.out.println("Cleared contents of "+ dir.getName());
+
+			System.out.println("Cleared contents of " + dir.getName());
+
+		}
+
+		String folders[] = { "client1", "client2", "storage-server1", "storage-server2", "storage-server3" };
+
+		for (String dirPath : folders) {
+			dir = new File(dirPath);
+			purgeDirectory(dir);
+
+			System.out.println("Cleared contents of " + dir.getName());
+
 		}
 
 		System.out.println("Done");
+	}
+
+	static void purgeDirectory(File dir) {
+		for (File file : dir.listFiles()) {
+			if (file.isDirectory())
+				purgeDirectory(file);
+			file.delete();
+		}
 	}
 }
