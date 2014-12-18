39ddf4a945e72967a86b10d6c166efe637d0d86e
Ivan Paner <ivan.paner@gmail.com>
2014-12-18 10:11:42 +0800

    Brute ported filerecords. Removed Connection since Jobs don't use them.

--- src/file/filerecords/FileRecord.java
+++ src/file/filerecords/FileRecord.java
@@ -1,4 +1,4 @@
-package actualclient.filerecords;
+package file.filerecords;
 
 import java.io.Serializable;
 import java.util.Date;
---
+++ src/file/filerecords/FolderRecord.java
@@ -0,0 +1,41 @@
+package file.filerecords;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class FolderRecord implements Serializable {
+	private ArrayList<FileRecord> list = new ArrayList<FileRecord>();
+	private long timeLastModified = 0; // time this folder record was last
+										// updated. this is just an internal
+										// time keeper used for when deletes
+										// happen while offline
+
+	public long getTimeLastModified() {
+		return timeLastModified;
+	}
+
+	public void setTimeLastModified(long timeLastModified) {
+		this.timeLastModified = timeLastModified;
+	}
+
+	@SuppressWarnings("unchecked")
+	public ArrayList<FileRecord> getList() {
+		return (ArrayList<FileRecord>) list.clone();
+	}
+
+	public void setList(ArrayList<FileRecord> list) {
+		this.list = list;
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder("Record as of " + new Date(this.timeLastModified).toString());
+
+		for (FileRecord record : list) {
+			sb.append(record.toString()).append("\n");
+		}
+
+		return sb.toString();
+	}
+
+}
